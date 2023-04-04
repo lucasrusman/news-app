af48427b64933172f00fb8ca33ce85f5f36874eb
lucasrusman <lucasrusman@sinergiasoluciones.com.ar>
2023-04-04 12:25:58 -0300

    news-app first commit

---
+++ lib/src/data/datasources/local/dao/article_dao.dart
@@ -0,0 +1,16 @@
+import 'package:floor/floor.dart';
+
+import '../../../../domain/models/article.dart';
+import '../../../../utils/constansts/strings.dart';
+
+@dao
+abstract class ArticleDao {
+  @Query('SELECT * FROM $articlesTableName')
+  Future<List<Article>> getAllArticles();
+
+  @Insert(onConflict: OnConflictStrategy.replace)
+  Future<void> insertArticle(Article article);
+
+  @delete
+  Future<void> deleteArticle(Article article);
+}
---
+++ lib/src/utils/constansts/strings.dart
@@ -0,0 +1,5 @@
+const String appTitle = 'Flutter Clean Architecture';
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = 'de177682f9754d0888838c1d0eb76ba2';
+const String articlesTableName = 'articles_table';
+const String databaseName = 'app_database.db';
